8bae29f0dcddf3d9a01708d121297f153c7d0d96
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2026-04-27 13:15:28 +0100

    Add a quick and dirty performance monitor

--- render-loop.js
+++ render-loop.js
@@ -2,6 +2,9 @@
 // requestAnimationFrame for non-video images has some subtlety to it
 // so this class handles the necessary tracking. If the scheduling method
 // is to be changed you must await .stop() and then call start()
+
+const MONITOR_PERFORMANCE = false
+
 export class RenderLoop {
   #renderCB
   #scheduleCB
@@ -18,7 +21,13 @@ export class RenderLoop {
     this.#renderCB = render
     this.#scheduleCB = schedule
     this.#cancelCB = cancel
-    this.#b_tick = this.#tick.bind(this)
+
+    if (MONITOR_PERFORMANCE) {
+      const monitor = new PerformanceMonitor()
+      this.#b_tick = () => monitor.measure(this.#tick.bind(this))
+    } else {
+      this.#b_tick = this.#tick.bind(this)
+    }
   }
 
   start () {
@@ -69,6 +78,40 @@ export class RenderLoop {
     if (this.#handle !== null) {
       this.#cancelCB(this.#handle)
       this.#handle = null
+    }
+  }
+}
+
+// n.b. This doesn't measure GPU Time
+class PerformanceMonitor {
+  constructor (interval = 1000) {
+    this.interval = interval
+    this.lastTime = performance.now()
+    this.frameCount = 0
+    this.totalDelta = 0
+    this.totalWork = 0
+  }
+
+  async measure (fn) {
+    const start = performance.now()
+    const delta = start - this.lastTime
+    this.lastTime = start
+
+    await fn()
+
+    const work = performance.now() - start
+    this.frameCount++
+    this.totalDelta += delta
+    this.totalWork += work
+
+    if (this.totalDelta >= this.interval) {
+      const fps = Math.round((this.frameCount * 1000) / this.totalDelta)
+      const avgF = (this.totalDelta / this.frameCount).toFixed(2)
+      const avgW = (this.totalWork / this.frameCount).toFixed(2)
+      console.log(`FPS: ${fps} | Frame: ${avgF}ms | Work: ${avgW}ms`)
+      this.frameCount = 0
+      this.totalDelta = 0
+      this.totalWork = 0
     }
   }
 }
